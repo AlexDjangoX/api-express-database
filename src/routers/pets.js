b6b3d599f65a8b15887930d65253320c30ca62a1
AlexDjangoX <alexmonk17@gmail.com>
2022-06-01 16:05:28 +0200

    part 1 pets complete

--- src/routers/pets.js
+++ src/routers/pets.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../db");
-
-// router.get("/", async (req, res) => {
-//   const result = await db.query('SELECT * FROM "pets";');
-//   res.json({ pets: result.rows });
-// });
+const microChipValue = ["true", "false"];
 
 router.get("/", async (req, res) => {
   let query = `SELECT * FROM "pets"`;
   const micro = req.query.microchip;
   const type = req.query.type;
 
+  if (micro && !microChipValue.includes(micro)) {
+    return res.status(400).json({ error: "microchip value invalid" });
+  }
+
   if (type) {
-    query = query + ` WHERE type = '${type}'`;
+    query += ` WHERE type='${type}'`;
     console.log(query);
   }
   if (micro) {
-    query = query + ` AND microchip = ${micro}`;
+    query += ` AND microchip='${micro}'`;
     console.log(query);
   }
 
@@ -43,6 +43,36 @@ router.post("/", async (req, res) => {
     return res.status(200).json({ pets: result.rows });
   } catch (e) {
     return res.status(400).json({ error: "Failed to propagate pet" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = { ...req.body };
+  const { name, age, type, breed, microchip } = data;
+  const values = [name, age, type, breed, microchip];
+  const sqlQueryString = `UPDATE pets SET name=$1, age=$2, 
+  type=$3, breed=$4, microchip=$5 WHERE id=${id} returning *`;
+
+  try {
+    const result = await db.query(sqlQueryString, values);
+    return res.status(200).json({ pet: result.rows[0] });
+  } catch (e) {
+    return res.status(400).json({ error: "Failed to update pet" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  const values = [id];
+  const sqlQueryString = `DELETE FROM pets WHERE id=$1 returning *`;
+  console.log(values);
+  console.log(sqlQueryString);
+  try {
+    const result = await db.query(sqlQueryString, values);
+    return res.status(200).json({ pet: result.rows[0] });
+  } catch (e) {
+    return res.status(400).json({ error: "Failed to delete pet" });
   }
 });
 
